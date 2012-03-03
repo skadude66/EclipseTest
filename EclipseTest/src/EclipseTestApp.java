6e7a479b2ea4379816a6cb9842a1ad7dc19c7bff
nick <nicholas.francoeur@gmail.com>
2012-03-02 22:31:01 -0500

    Initial Commit

---
+++ EclipseTest/src/EclipseTestApp.java
@@ -0,0 +1,11 @@
+
+public class EclipseTestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");	}
+
+}
